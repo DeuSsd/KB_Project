736d74c7642502ab53a53d588f9bceffb34eff20
Denis Ognev <den.is.ognev@yandex.ru>
2021-02-24 14:28:45 +0300

    Сделал выбор интересующей машины

--- KB_Module.py
+++ KB_Module.py
@@ -216,178 +216,221 @@ input_names = convert_dict_to_ilst(parametrs_of_function)  # список вхо
 output_names = [exit_parametr]  # список выходных параметров
 
 # ////////////////////////////////////////////////////////////////////////////////
+#
+# while True:
+#     # узнаём есть ли нейросетевые модели в базе и если есть забираем последнюю
+#     q = g.query(
+#         '''
+#         PREFIX URN: <file:///U:/7%20%D1%81%D0%B5%D0%BC%D0%B5%D1%81%D1%82%D1%80/pythonProject/MyBase/#>
+#         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+#
+#         SELECT ?instance
+#         WHERE
+#         {
+#             ?instance a URN:NN_Model .
+#             ?instance URN:active true .
+#         }
+#         ''')
+#
+#     # затестить
+#     # def objects(self, subject=None, predicate=None):
+#     #     """A generator of objects with the given subject and predicate"""
+#     #     formula s, p, o in self.triples((subject, predicate, None)):
+#     #         yield o
+#
+#     result = False
+#     base_nn_model_name = ""
+#     nn_model_name = ""
+#     if len(q) == 0:
+#         print("нейросетевой модели нет")
+#         q = g.query(
+#             '''
+#             PREFIX URN: <file:///U:/7%20%D1%81%D0%B5%D0%BC%D0%B5%D1%81%D1%82%D1%80/pythonProject/MyBase/#>
+#             SELECT ?model_name
+#             WHERE
+#             {
+#                 URN:NN_model URN:base_model_name ?model_name.
+#             }
+#             '''
+#         )
+#         for item in q:
+#             # print(item)
+#             # name = item[0].split("#")[1]
+#             base_nn_model_name = item[0]
+#             # print(name)
+#         base_nn_model_name += "1"
+#         print(base_nn_model_name)
+#         nn_model_name = os.path.join(path_nn, base_nn_model_name + ".h5")
+#         print(nn_model_name)
+#         # обучение нейросети
+#         accuracy = 0
+#         while accuracy < 0.3:
+#
+#             learn_nn.learn_nn(
+#                 nn_model_name,
+#                 input_names,
+#                 output_names,
+#                 "data.csv",
+#                 number_of_neurons,
+#                 number_of_layer
+#             )
+#
+#             accuracy = test_nn(
+#                 nn_model_name,
+#                 input_names,
+#                 output_names,
+#                 formula_name,
+#                 "data.csv"
+#             )
+#             print(number_of_neurons,number_of_layer,accuracy)
+#             if number_of_neurons < 0:
+#                 number_of_neurons+=1
+#             elif number_of_layer < 0:
+#                 number_of_layer += 1
+#             else:
+#                 break
+#
+#         add_new_nn_to_KB(nn_model_name)
+#
+#     else:
+#         print("нейросетевая модель есть")
+#         print(len(q))
+#         for item in q:
+#             nn_model_name = item[0].split("#")[1]
+#             print(nn_model_name)
+#
+#         q = g.query(
+#             '''
+#             PREFIX URN: <file:///U:/7%20%D1%81%D0%B5%D0%BC%D0%B5%D1%81%D1%82%D1%80/pythonProject/MyBase/#>
+#             PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+#
+#             SELECT ?Name
+#             WHERE
+#             {
+#                 URN:''' + nn_model_name + ''' URN:model_name ?Name .
+#                 URN:''' + nn_model_name + ''' URN:active true  .
+#             }
+#             ''')
+#
+#         for item in q:
+#             nn_model_name = item[0]
+#             print(nn_model_name)
+#
+#         full_path_nn_model = os.path.join(path_nn, nn_model_name + ".h5")
+#         print(full_path_nn_model)
+#
+#         accuracy = test_nn(
+#                     full_path_nn_model,
+#                     input_names,
+#                     output_names,
+#                     formula_name,
+#                     "data.csv"
+#                 ) #проверка нейросети
+#         print(number_of_neurons, number_of_layer, accuracy)
+#         if accuracy < 0.3:
+#             relearn_nn.relearn_nn(
+#                 full_path_nn_model,
+#                 input_names,
+#                 output_names,
+#                 "data.csv"
+#             )
+#
+#             accuracy = test_nn(
+#                 full_path_nn_model,
+#                 input_names,
+#                 output_names,
+#                 formula_name,
+#                 "data.csv"
+#             )  # проверка нейросети
+#             print(number_of_neurons, number_of_layer, accuracy)
+#             if accuracy < 0.3:
+#                 name,i_num = nn_model_name.split("_")
+#                 name += "_{}".format(int(i_num)+1)
+#                 nn_model_name = os.path.join(path_nn, name + ".h5")
+#                 # обучение нейросети
+#                 accuracy = 0
+#                 while accuracy < 0.3:
+#
+#                     learn_nn.learn_nn(
+#                         nn_model_name,
+#                         input_names,
+#                         output_names,
+#                         "data.csv",
+#                         number_of_neurons,
+#                         number_of_layer
+#                     )
+#
+#                     accuracy = test_nn(
+#                         nn_model_name,
+#                         input_names,
+#                         output_names,
+#                         formula_name,
+#                         "data.csv"
+#                     )
+#
+#                     print(number_of_neurons, number_of_layer, accuracy)
+#                     if number_of_neurons < 0:
+#                         number_of_neurons += 1
+#                     elif number_of_layer < 0:
+#                         number_of_layer += 1
+#                     else:
+#                         break
+#                 add_new_nn_to_KB(nn_model_name)
+#                 print(2,nn_model_name)
+#             else:
+#                 print(1)
+#                 change_status(nn_model_name)
+#
+#     # Запись в базу знаний, сохранение
+#     file = open("KB.n3", mode="wb")
+#     file.write(g.serialize(format="n3"))
+#     file.close()
+#     # if input():
+#     #     break
 
-while True:
-    # узнаём есть ли нейросетевые модели в базе и если есть забираем последнюю
-    q = g.query(
-        '''
-        PREFIX URN: <file:///U:/7%20%D1%81%D0%B5%D0%BC%D0%B5%D1%81%D1%82%D1%80/pythonProject/MyBase/#>
-        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    # print("\033[36mГраф имеет {} триплетов!".format(len(g)))
+    # # print("\033[35m {} !".format([i formula i in g.namespaces()]))
+    # break
+# ////////////////////////////////////////////////////////////////////////////////
 
-        SELECT ?instance
-        WHERE
-        {
-            ?instance a URN:NN_Model .
-            ?instance URN:active true .
-        }
-        ''')
-
-    # затестить
-    # def objects(self, subject=None, predicate=None):
-    #     """A generator of objects with the given subject and predicate"""
-    #     formula s, p, o in self.triples((subject, predicate, None)):
-    #         yield o
-
-    result = False
-    base_nn_model_name = ""
-    nn_model_name = ""
-    if len(q) == 0:
-        print("нейросетевой модели нет")
-        q = g.query(
-            '''
-            PREFIX URN: <file:///U:/7%20%D1%81%D0%B5%D0%BC%D0%B5%D1%81%D1%82%D1%80/pythonProject/MyBase/#>
-            SELECT ?model_name
-            WHERE
-            {
-                URN:NN_model URN:base_model_name ?model_name.
-            }
-            '''
-        )
-        for item in q:
-            # print(item)
-            # name = item[0].split("#")[1]
-            base_nn_model_name = item[0]
-            # print(name)
-        base_nn_model_name += "1"
-        print(base_nn_model_name)
-        nn_model_name = os.path.join(path_nn, base_nn_model_name + ".h5")
-        print(nn_model_name)
-        # обучение нейросети
-        accuracy = 0
-        while accuracy < 0.3:
-
-            learn_nn.learn_nn(
-                nn_model_name,
-                input_names,
-                output_names,
-                "data.csv",
-                number_of_neurons,
-                number_of_layer
-            )
-
-            accuracy = test_nn(
-                nn_model_name,
-                input_names,
-                output_names,
-                formula_name,
-                "data.csv"
-            )
-            print(number_of_neurons,number_of_layer,accuracy)
-            if number_of_neurons < 0:
-                number_of_neurons+=1
-            elif number_of_layer < 0:
-                number_of_layer += 1
-            else:
-                break
-
-        add_new_nn_to_KB(nn_model_name)
-
-    else:
-        print("нейросетевая модель есть")
-        print(len(q))
-        for item in q:
-            nn_model_name = item[0].split("#")[1]
-            print(nn_model_name)
-
-        q = g.query(
-            '''
+q = g.query('''
             PREFIX URN: <file:///U:/7%20%D1%81%D0%B5%D0%BC%D0%B5%D1%81%D1%82%D1%80/pythonProject/MyBase/#>
             PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 
-            SELECT ?Name
+            SELECT ?Object ?Name
             WHERE
             {
-                URN:''' + nn_model_name + ''' URN:model_name ?Name .
-                URN:''' + nn_model_name + ''' URN:active true  .
+                ?Object a MyBase:hardware.
+                ?Object MyBase:name ?Name.
             }
-            ''')
+''')
 
-        for item in q:
-            nn_model_name = item[0]
-            print(nn_model_name)
+hardware_list = {}
+num = 1
+for item,name in q:
+    hardware_list[num] = (item.split("#")[-1],str(name))
+    num+=1
+num-=1
+print(hardware_list)
 
-        full_path_nn_model = os.path.join(path_nn, nn_model_name + ".h5")
-        print(full_path_nn_model)
+# file = open("KB.n3", mode="wb")
+# file.write(g.serialize(format="n3"))
+# file.close()
+print("num: ",num)
+for i in range(num):
+    print("["+str(i+1)+"] - "+hardware_list[i+1][-1])
+number_of_picked_system = int(input("Выберите систему под номером: "))
+picked_system = hardware_list[number_of_picked_system][0]
+q = g.query('''
+            PREFIX URN: <file:///U:/7%20%D1%81%D0%B5%D0%BC%D0%B5%D1%81%D1%82%D1%80/pythonProject/MyBase/#>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 
-        accuracy = test_nn(
-                    full_path_nn_model,
-                    input_names,
-                    output_names,
-                    formula_name,
-                    "data.csv"
-                ) #проверка нейросети
-        print(number_of_neurons, number_of_layer, accuracy)
-        if accuracy < 0.3:
-            relearn_nn.relearn_nn(
-                full_path_nn_model,
-                input_names,
-                output_names,
-                "data.csv"
-            )
+            SELECT ?p ?s
+            WHERE
+            {
+                URN:'''+picked_system+''' ?p ?s.
+            }
+''')
 
-            accuracy = test_nn(
-                full_path_nn_model,
-                input_names,
-                output_names,
-                formula_name,
-                "data.csv"
-            )  # проверка нейросети
-            print(number_of_neurons, number_of_layer, accuracy)
-            if accuracy < 0.3:
-                name,i_num = nn_model_name.split("_")
-                name += "_{}".format(int(i_num)+1)
-                nn_model_name = os.path.join(path_nn, name + ".h5")
-                # обучение нейросети
-                accuracy = 0
-                while accuracy < 0.3:
-
-                    learn_nn.learn_nn(
-                        nn_model_name,
-                        input_names,
-                        output_names,
-                        "data.csv",
-                        number_of_neurons,
-                        number_of_layer
-                    )
-
-                    accuracy = test_nn(
-                        nn_model_name,
-                        input_names,
-                        output_names,
-                        formula_name,
-                        "data.csv"
-                    )
-
-                    print(number_of_neurons, number_of_layer, accuracy)
-                    if number_of_neurons < 0:
-                        number_of_neurons += 1
-                    elif number_of_layer < 0:
-                        number_of_layer += 1
-                    else:
-                        break
-                add_new_nn_to_KB(nn_model_name)
-                print(2,nn_model_name)
-            else:
-                print(1)
-                change_status(nn_model_name)
-
-    # Запись в базу знаний, сохранение
-    file = open("KB.n3", mode="wb")
-    file.write(g.serialize(format="n3"))
-    file.close()
-    # if input():
-    #     break
-
-    print("\033[36mГраф имеет {} триплетов!".format(len(g)))
-    # print("\033[35m {} !".format([i formula i in g.namespaces()]))
-    break
+for p,s in q:
+    print(p.split("#")[-1],s)
